5d48e1803e1587a06cea790d2b49723e1b6bf433
karan <karangavali64@gmail.com>
2022-10-20 13:33:58 +0530

    insertion sort added

---
+++ SortingAlgorithms/insertionSort.py
@@ -0,0 +1,14 @@
+def insertionSort(a):
+    n=len(a)
+    #we assume first element is already sorted hence comaprision starts from 1st element
+    for i in range(1,n):
+        # cval=a[i]
+        pos=i
+        # position must be greater than 0 and position-1 element must be greater than its right element i.e. position element(cval)
+        while pos>0 and a[pos-1]>a[pos]:
+            a[pos],a[pos-1]=a[pos-1],a[pos]
+            pos-=1
+        # a[pos]=cval
+l=[5,2,-1,0,10]
+insertionSort(l)
+print(l)
